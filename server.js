da15dea0fb3d7ee910a2a9579469c73420132099
Suraj Sabu <suraj28.nxs@gmail.com>
2026-03-13 11:05:54 +0530

    add package lock

--- server.js
+++ server.js
@@ -31,3 +31,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`DIN lookup server running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('DIN Lookup API is running!');
+});
